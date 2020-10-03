6874af6c59bdc0eafb0714002d2b135dbcb3e892
kshitijmb <62787321+kshitijmb@users.noreply.github.com>
2020-10-03 16:47:45 +0530

    Update get_data.sh

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 mkdir ../data
 wget http://sketch-code.s3.amazonaws.com/data/all_data.zip -O ../data/all_data.zip
-unzip ../data/all_data.zip -d ../data/all_data
+cd data
+unzip ../all_data.zip -d ../all_data
